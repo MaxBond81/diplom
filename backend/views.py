a464808f24d83d6ab42573d7c67b1b1b4684b42d
Bondarev Maxim <bonmax46@mail.ru>
2025-09-26 19:58:36 +0300

    add do_import celery

--- backend/views.py
+++ backend/views.py
@@ -20,6 +20,8 @@ from backend.serializers import UserSerializer, CategorySerializer, ShopSerializ
     OrderItemSerializer, OrderSerializer, ContactSerializer
 from backend.signals import new_user_registered, new_order
 
+from backend.tasks import do_import, send_email
+
 
 def string_to_bool(value: str) -> bool:
     """
@@ -439,59 +441,32 @@ class PartnerUpdate(APIView):
     - None
     """
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs):
         """
-                Update the partner price list information.
+        Обновление прайса от поставщика
 
-                Args:
-                - request (Request): The Django request object.
+        Args:
+            request: HTTP-запрос
+            *args: Дополнительные аргументы
+            **kwargs: Дополнительные аргументы
 
-                Returns:
-                - JsonResponse: The response indicating the status of the operation and any errors.
-                """
-        if not request.user.is_authenticated:
-            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+        Returns:
+            JsonResponse: JSON-ответ с результатом обновления прайса от поставщика
+        """
 
         if request.user.type != 'shop':
-            return JsonResponse({'Status': False, 'Error': 'Only for shops'}, status=403)
+            return JsonResponse({'Status': False, 'Error': 'Эта функция только для магазинов'}, status=403)
 
         url = request.data.get('url')
         if url:
             validate_url = URLValidator()
             try:
                 validate_url(url)
-            except ValidationError as e:
-                return JsonResponse({'Status': False, 'Error': str(e)})
-            else:
-                stream = get(url).content
-
-                data = load_yaml(stream, Loader=Loader)
-
-                shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
-                for category in data['categories']:
-                    category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
-                    category_object.shops.add(shop.id)
-                    category_object.save()
-                ProductInfo.objects.filter(shop_id=shop.id).delete()
-                for item in data['goods']:
-                    product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
-
-                    product_info = ProductInfo.objects.create(product_id=product.id,
-                                                              external_id=item['id'],
-                                                              model=item['model'],
-                                                              price=item['price'],
-                                                              price_rrc=item['price_rrc'],
-                                                              quantity=item['quantity'],
-                                                              shop_id=shop.id)
-                    for name, value in item['parameters'].items():
-                        parameter_object, _ = Parameter.objects.get_or_create(name=name)
-                        ProductParameter.objects.create(product_info_id=product_info.id,
-                                                        parameter_id=parameter_object.id,
-                                                        value=value)
-
-                return JsonResponse({'Status': True})
-
-        return JsonResponse({'Status': False, 'Errors': 'All necessary arguments are not specified'})
+            except ValidationError:
+                return JsonResponse({'Status': False, 'Error': 'Неверный URL'}, status=400)
+            task = do_import.delay(url, user_id=request.user.id)
+            return JsonResponse({'Status': True, 'Task': str(task), 'Task ID': task.id})
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'}, status=400)
 
 
 class PartnerState(APIView):
